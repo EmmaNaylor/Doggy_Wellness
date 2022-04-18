c7f579cbdf5d46910fb5096196c6876e219b6ecd
razrobill <tb1126@york.ac.uk>
2022-04-18 09:43:12 +0100

    updated user story 3

--- Userstory3.py
+++ Userstory3.py
@@ -1,14 +1,22 @@
 #user story 3 - as a class instructor, I want to be able to limit the number of participants in each class
 
+#this is a template for what it would look like
 
-j=0
-while j <= 20:
-    user = input("would you like to go to class? y/n")
-    if user == "y":
-        j = j+1
-        #print(j)
+
+def class_fullness():
+    j=0
+    while j <= 20:
+        user = input("would you like to attend this class? y/n")
+        if user == "y":
+            j = j+1
+            print(j)
     if j > 20:
-        print("class is full")
+        raise Exception("sorry, this class is now full")
+
+
+if __name__ == "__main__":
+    class_fullness()
+
 
 
 
